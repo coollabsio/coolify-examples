fdf879c400963adfa70ed3ba8c8991a5b94c5944
Andras Bacsai <andras.bacsai@protonmail.com>
2023-07-04 14:26:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const fastify = require('fastify')({ logger: {level: "error"}, trustProxy: true 
 fastify.register(require('fastify-cors'))
 
 fastify.get('/', function (req, reply) {
+  return { hello: "nodesjs-fastify branch" }
+})
+fastify.get('/env', function (req, reply) {
   console.log(process.env.TEST)
-  return { hello: "main-updates" }
+  return { TEST: process.env.TEST ?? "TEST variable not found." }
 })
 
 fastify.get('/health', function (req, reply) {
