3e20c9cea5f385c1d6bbfc32b6db5889d0295ab1
Andras Bacsai <andras.bacsai@protonmail.com>
2024-03-19 10:58:07 +0100

    Update index.ts

--- docker-compose/index.ts
+++ docker-compose/index.ts
@@ -5,7 +5,7 @@ Bun.serve({
     port: PORT,
     fetch(req) {
         const url = new URL(req.url);
-        if (url.pathname === "/") return new Response("Home page!");
+        if (url.pathname === "/") return new Response("Home page pr!");
         if (url.pathname === "/envs") {
             return new Response(JSON.stringify(process.env, null, 2), {
                 headers: { "content-type": "application/json" },
