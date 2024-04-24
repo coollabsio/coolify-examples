556fcb3019f94a1a6caadca704234841d0082b1b
Andras Bacsai <andras.bacsai@gmail.com>
2024-04-24 11:14:46 +0200

    Refactor image loader URL and optimize image in loader.js

--- nextjs/spa-with-image-optimization/loader.js
+++ nextjs/spa-with-image-optimization/loader.js
@@ -2,16 +2,22 @@
 
 export default function myImageLoader({ src, width, quality }) {
     const isLocal = !src.startsWith('http');
+    const query = new URLSearchParams();
+
+    const imageOptimizationApi = 'https://images.coollabs.io/o';
+    // Your NextJS application URL
+    const baseUrl = 'https://xoos0kk.heyandras.dev';
+
+    const fullSrc = `${baseUrl}${src}`;
+
+    if (width) query.set('width', width);
+    if (quality) query.set('quality', quality);
+
     if (isLocal && process.env.NODE_ENV === 'development') {
         return src;
     }
-    const query = new URLSearchParams();
-    if (width) query.set('width', width);
-    if (quality) query.set('quality', quality);
     if (isLocal) {
-        const baseUrl = 'https://xoos0kk.heyandras.dev';
-        const fullSrc = `${baseUrl}${src}`;
-        return `https://images.coollabs.io/o/${fullSrc}?${query.toString()}`;
+        return `${imageOptimizationApi}/${fullSrc}?${query.toString()}`;
     }
-    return `https://images.coollabs.io/o/${src}?${query.toString()}`;
+    return `${imageOptimizationApi}/${src}?${query.toString()}`;
 }
