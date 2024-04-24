35363e21cc8c8c27674ad092eb9000a1d0089542
Andras Bacsai <andras.bacsai@gmail.com>
2024-04-24 09:30:45 +0200

    Update image loader URL in loader.js

--- nextjs/spa-with-image-optimization/loader.js
+++ nextjs/spa-with-image-optimization/loader.js
@@ -8,7 +8,7 @@ export default function myImageLoader({ src, width, quality }) {
     if (isLocal) {
         const baseUrl = 'https://xoos0kk.heyandras.dev';
         const fullSrc = `${baseUrl}${src}`;
-        return `https://images.coollabs.io/o/${encodeURIComponent(fullSrc)}?width=${width}&quality=${quality}`
+        return `https://images.coollabs.io/o/${fullSrc}?width=${width}&quality=${quality || 75}`
     }
-    return `https://images.coollabs.io/o/${encodeURIComponent(src)}?width=${width}&quality=${quality}`
+    return `https://images.coollabs.io/o/${src}?width=${width}&quality=${quality|| 75}`
 }
