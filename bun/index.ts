0f87f6b5d8d067ff9427c0a68912e13d790e3d66
Andras Bacsai <andras.bacsai@protonmail.com>
2024-05-15 09:56:48 +0200

    Update index.ts

--- bun/index.ts
+++ bun/index.ts
@@ -5,6 +5,7 @@ Bun.serve({
         const url = new URL(req.url);
         if (url.pathname === "/") return new Response("Home page!");
         if (url.pathname === "/blog") return new Response("Blog!");
+        if (url.pathname === "/about") return new Response("About!");
         return new Response("404!");
     },
 })
