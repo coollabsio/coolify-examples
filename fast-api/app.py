2bca72f7adf3785a75aa783c8ce213bf8e41ed3a
Timothy Olaleke <timothyolaleke@gmail.com>
2024-07-09 22:18:21 +0100

    fix port

--- fast-api/app.py
+++ fast-api/app.py
@@ -88,4 +88,4 @@ def add_domain(config: TraefikConfig):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5555)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
