14025b60a47470b096c3f6c36ab9ede20c9956f4
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-09-15 14:15:44 +0200

    Update build.sh

--- docker-compose/scripts/build.sh
+++ docker-compose/scripts/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "✔️ directory structure created, starting the build..."
 
-docker compose -f ./docker-compose.coolify__generated.yml build
+docker compose -f ./docker-compose/docker-compose-test.yaml build
