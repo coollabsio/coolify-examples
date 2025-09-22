942c03b36dd7645e8b8eb1b7e85c869f91dcc411
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-09-22 20:16:32 +0200

    adonisjs

---
+++ adonisjs/simple/start/routes.ts
@@ -0,0 +1,12 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.on('/').render('pages/home')
