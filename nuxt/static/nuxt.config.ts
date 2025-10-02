34e6d4dc87dfee5f5fd869c9f2717f306a5602ff
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-10-02 11:33:38 +0200

    Update nuxt.config.ts

--- nuxt/static/nuxt.config.ts
+++ nuxt/static/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
+  target: 'static',
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
