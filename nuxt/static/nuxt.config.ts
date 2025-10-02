e3433908ee7250a607460ba2792ed88e620232e1
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-10-02 11:27:41 +0200

    asdf

--- nuxt/static/nuxt.config.ts
+++ nuxt/static/nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      appUrl: process.env.APP_URL || 'something'
+    }
+  }
 })
