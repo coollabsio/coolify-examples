de88a8afb36f60d98d9a14a26ba0e160aa6d0e76
Andras Bacsai <5845193+andrasbacsai@users.noreply.github.com>
2025-12-08 16:55:13 +0100

    Update index.ts

--- bun/index.ts
+++ bun/index.ts
@@ -6,7 +6,7 @@ Bun.serve({
         if (url.pathname === "/") return new Response("Home page!");
         if (url.pathname === "/blog") return new Response("Blog!");
         if (url.pathname === "/about") return new Response("About!");
-        if (url.pathname === "/x") return new Response("a");
+        if (url.pathname === "/x") return new Response("b");
         if (url.pathname === "/201") return new Response("201", { status: 201 });
         if (url.pathname === "/202") return new Response("202", { status: 202 });
         return new Response("404!");
